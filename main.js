c07689efd60440acb14e09a7ab2fd7db81817ac6
xigim <147914113+xigim@users.noreply.github.com>
2023-10-14 16:25:59 +0700

    Add files via upload

---
+++ main.js
@@ -0,0 +1,8 @@
+let nama = prompt('siapa nama anda: ')
+let usia = prompt('berapa usia anda: ')
+let tinggiBadan = prompt('berapa tinggi badan anda: ')
+
+
+alert (`nama anda adalah ${nama} anda berusia ${usia} dan anda memiliki tinggi badan ${tinggiBadan}`)
+
+console.log(alert)
